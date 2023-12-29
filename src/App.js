91c9088daf708ed16bb8b9c8059a8a69857cbda1
Raymond Z <raykyri@gmail.com>
2023-12-29 01:03:30 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ function App() {
       <p className="mb-2">
         <span className="font-bold text-xl">raymond</span>
       </p>
-      <p>Hi!</p>
-      <p>I’m a researcher and software engineer.</p>
       <p>
-        I work on{" "}
+        Hi! I’m a researcher and software engineer, currently working on a new
+        runtime for decentralized applications, as a founder at{" "}
         <a target="_blank" href="https://canvas.xyz" rel="noreferrer">
           Canvas
         </a>
-        , a peer-to-peer technology company.
+        .
       </p>
       <p>
-        In the past, I was a co-founder at{" "}
+        Previously I was a co-founder at{" "}
         <a target="_blank" href="https://commonwealth.im" rel="noreferrer">
           Commonwealth
         </a>
-        , building governance tools for crypto networks.
+        , where we built protocol governance tools for crypto networks.
+        Commonwealth spun out into a separate company in 2021, which raised
+        $20m+ from top investors including Dragonfly, Polychain, Framework, and
+        Spark Capital.
+      </p>
+      <p>
+        I designed and led engineering for the v1 product at Commonwealth, and
+        help out with other early-stage projects in engineering and design.{" "}
+      </p>
+      <p>
+        I also occasionally angel invest; past investments include the earliest
+        rounds of Retool, Roam, Secureframe, Slope, Mem Protocol, Capsule, and
+        Worldcoin.
+      </p>
+      <p>
+        I also read a lot; some of my favorite writers include Ada Palmer, Byung
+        Chul-Han, and Yuk Hui.
       </p>
     </div>
   )
